d8a00d299e8c39f74cf21cd6e82f3a9745e2ab82
KaterinaSokolovskaya <ekaterina.sokol.20@yandex.com>
2021-06-28 10:58:31 +0300

    3 commit

---
+++ src/main/java/com/sokolovskaya/sectionsservices/model/Category.java
@@ -0,0 +1,19 @@
+package com.sokolovskaya.sectionsservices.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Category {
+
+    String name;
+
+    Set<String> services;
+}
